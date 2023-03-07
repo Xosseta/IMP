65e976d8d702c1f51af247f669dc8df804cb07d1
Xosseta <118289878+Xosseta@users.noreply.github.com>
2023-03-07 21:15:08 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from application.salary import calculate_salary
+from application.db.people import get_employees
+import antigravity
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    print(datetime.now())
+
+
+
+
